aba431112e61a3d728b25e69025fb4b3d9935d34
Craig Ferry <PurityControl@users.noreply.github.com>
2014-09-29 22:03:07 +0000

    use a transform to capture the cash amount

--- atm/features/step_definitions/basic_steps.rb
+++ atm/features/step_definitions/basic_steps.rb
@@ -8,17 +8,21 @@ class Account
   end
 end
 
-Given /^I have deposited \$(\d+) in my account$/ do |amount|
+CAPTURE_CASH_AMOUNT = Transform /^\$\d+$/ do |number|
+  number.to_i
+end
+
+Given /^I have deposited (#{CAPTURE_CASH_AMOUNT}) in my account$/ do |amount|
   my_account = Account.new
-  my_account.deposit(amount.to_i)
-  expect(my_account.balance).to eq(amount.to_i),
+  my_account.deposit(amount)
+  expect(my_account.balance).to eq(amount),
     "Expected the balance to be #{amount} but it was #{my_account.balance}"
 end
 
-When /^I request \$(\d+)$/ do |arg1|
+When /^I request (#{CAPTURE_CASH_AMOUNT})$/ do |arg1|
   pending # express the regexp above with the code you wish you had
 end
 
-Then /^\$(\d+) should be dispensed$/ do |arg1|
+Then /^(#{CAPTURE_CASH_AMOUNT}) should be dispensed$/ do |arg1|
   pending # express the regexp above with the code you wish you had
 end
