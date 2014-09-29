0c5840ad6cd850bf0e833a09044e41cf2529350b
Craig Ferry <PurityControl@users.noreply.github.com>
2014-09-29 22:42:04 +0100

    added conditional to only install rvm et. al. if not already installed

--- vagrant-trusty64-mean.sh
+++ vagrant-trusty64-mean.sh
@@ -1,23 +1,30 @@
 sudo apt-get update
 sudo apt-get install -y git tree mc emacs24-nox 
 
-#download and install rvm
-\curl -sSL https://get.rvm.io | bash -s stable
+# install rvm and install ruby if not provisioned before
+which rvm
+if [ $? -ne 0 ]
+then
+  #download and install rvm
+  \curl -sSL https://get.rvm.io | bash -s stable
 
-# load rvm on login
-echo '' >> ~/.bash_profile
-echo '#source profile for rvm' >> ~/.bash_profile
-echo 'source ~/.profile' >> ~/.bash_profile
+  # load rvm on login
+  echo '' >> ~/.bash_profile
+  echo '#source profile for rvm' >> ~/.bash_profile
+  echo 'source ~/.profile' >> ~/.bash_profile
 
-#load rvm to run now
-. ~/.bash_profile
+  #load rvm to run now
+  . ~/.bash_profile
 
-# install all packages needed to let rvm do its job
-rvm requirements run
+  # install all packages needed to let rvm do its job
+  rvm requirements run
 
-# install ruby and bundler
-rvm install ruby
-gem install bundler
+  # install ruby and bundler
+  rvm install ruby
+  gem install bundler
+else
+  echo 'rvm already installed skipping...'
+fi
 
 # install gems in atm folder
 cd /vagrant/atm
