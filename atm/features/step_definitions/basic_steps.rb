f1bc7643a693f2dabba4ce9a5c7848db3ccd5945
Craig Ferry <PurityControl@users.noreply.github.com>
2014-10-01 22:12:02 +0000

    get withdraw functionality passing

--- atm/features/step_definitions/basic_steps.rb
+++ atm/features/step_definitions/basic_steps.rb
@@ -8,21 +8,34 @@ class Account
   end
 end
 
+class Teller
+  def withdraw_from(account, amount)
+  end
+end
+
 CAPTURE_CASH_AMOUNT = Transform /^\$\d+$/ do |number|
   number.to_i
 end
 
+module KnowsMyAccount
+  def my_account
+    @my_account  ||= Account.new
+  end
+end
+
+World(KnowsMyAccount)
+
 Given /^I have deposited (#{CAPTURE_CASH_AMOUNT}) in my account$/ do |amount|
-  my_account = Account.new
   my_account.deposit(amount)
   expect(my_account.balance).to eq(amount),
     "Expected the balance to be #{amount} but it was #{my_account.balance}"
 end
 
-When /^I request (#{CAPTURE_CASH_AMOUNT})$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
+When /^I withdraw (#{CAPTURE_CASH_AMOUNT})$/ do |amount|
+  teller = Teller.new
+  teller.withdraw_from(my_account, amount)
 end
 
-Then /^(#{CAPTURE_CASH_AMOUNT}) should be dispensed$/ do |arg1|
+Then /^(#{CAPTURE_CASH_AMOUNT}) should be dispensed$/ do |amount|
   pending # express the regexp above with the code you wish you had
 end
