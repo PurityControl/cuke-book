4e7cffe040dc97ebdea75eb42f2e5aa732112228
Craig Ferry <PurityControl@users.noreply.github.com>
2014-10-03 08:36:16 +0000

    moved teller steps into its own file

--- atm/features/step_definitions/basic_steps.rb
+++ atm/features/step_definitions/basic_steps.rb
@@ -1,7 +1,3 @@
-When /^I withdraw (#{CAPTURE_CASH_AMOUNT})$/ do |amount|
-  teller.withdraw_from(my_account, amount)
-end
-
 Then /^(#{CAPTURE_CASH_AMOUNT}) should be dispensed$/ do |amount|
   expect(cash_slot.contents).to eq amount
 end
