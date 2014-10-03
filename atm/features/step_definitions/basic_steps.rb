9457f35b7695ead43f2c50608889bb8c0bf1e3ce
Craig Ferry <PurityControl@users.noreply.github.com>
2014-10-03 08:34:17 +0000

    moved account step into its own file

--- atm/features/step_definitions/basic_steps.rb
+++ atm/features/step_definitions/basic_steps.rb
@@ -1,9 +1,3 @@
-Given /^I have deposited (#{CAPTURE_CASH_AMOUNT}) in my account$/ do |amount|
-  my_account.deposit(amount)
-  expect(my_account.balance).to eq(amount),
-    "Expected the balance to be #{amount} but it was #{my_account.balance}"
-end
-
 When /^I withdraw (#{CAPTURE_CASH_AMOUNT})$/ do |amount|
   teller.withdraw_from(my_account, amount)
 end
